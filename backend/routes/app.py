ebc633ad3963b8aea414bef85f0654af6015c5a3
casavotantan@gmail.com <casavotantan@gmail.com>
2026-05-26 22:18:50 -0300

    aaaa4

--- backend/routes/app.py
+++ backend/routes/app.py
@@ -13,7 +13,7 @@ CORS(app)
 
 def conectar():
 
-    conn = sqlite3.connect("backend/banco/banco.db")
+    conn = sqlite3.connect("backend/banco.db")
     conn.row_factory = sqlite3.Row
 
     return conn
